912cf4488524cd40648c539690944aaeb651b47a
nikhita <nikhitajamadar1998@gmail.com>
2022-01-08 13:07:28 -0800

    Displaying welcome to line comparison program

---
+++ LineComparison.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    class LineComparison
+    {
+        public static void LineComparisonProgram()
+        {
+            Console.WriteLine("Welcome to line comparison computation program");
+        }
+    }
+}
